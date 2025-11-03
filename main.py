1ec0d35978624d63ccbbdd182f9c6ac5f7d044cc
Thilo Schaper <thilo.schaper@cariad.technology>
2025-11-03 15:18:14 +0100

    fix step get_data using local workflow and removing pyarrow dependency

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-
-import mlflow
-import tempfile
 import os
-import wandb
+import tempfile
+
 import hydra
+import mlflow
+import wandb
 from omegaconf import DictConfig
 
 _steps = [
@@ -16,37 +16,40 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    "test_regression_model"
 ]
 
 
 # This automatically reads in the configuration
-@hydra.main(version_base=None, config_name='config')  # Adding version_base for Python 3.13 compatibility
+@hydra.main(
+    # version_base=None, config_name="config"
+    version_base=None,
+    config_name="config",
+    config_path=".",
+)  # Adding version_base for Python 3.13 compatibility
 def go(config: DictConfig):
-
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
     # Steps to execute
-    steps_par = config['main']['steps']
+    steps_par = config["main"]["steps"]
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
     # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
-
         if "download" in active_steps:
             # Download file and load in W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
-                version='main',
-                env_manager="conda",
+                # version="main",
+                # env_manager="conda",
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    "artifact_description": "Raw file as downloaded",
                 },
             )
 
@@ -54,6 +57,7 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+            print(config)
             pass
 
         if "data_check" in active_steps:
@@ -69,11 +73,12 @@ def go(config: DictConfig):
             pass
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+                json.dump(
+                    dict(config["modeling"]["random_forest"].items()), fp
+                )  # DO NOT TOUCH
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
@@ -85,7 +90,6 @@ def go(config: DictConfig):
             pass
 
         if "test_regression_model" in active_steps:
-
             ##################
             # Implement here #
             ##################
