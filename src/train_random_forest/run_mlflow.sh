84ec646cbe236ac486d0f08b437f3d8a327c36e2
Thilo Schaper <thilo.schaper@cariad.technology>
2025-11-06 15:12:37 +0100

    finish train random forest step

---
+++ src/train_random_forest/run_mlflow.sh
@@ -0,0 +1,5 @@
+pushd ../..
+mlflow run . \
+  -P steps=train_random_forest \
+  -P hydra_options="modeling.random_forest.max_depth=10,50,100 modeling.random_forest.n_estimators=100,200,500 -m"
+popd
