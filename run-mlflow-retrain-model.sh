1faa675aea6bb59b20240368c06e98fb69c304e6
Thilo Schaper <thilo.schaper@cariad.technology>
2025-11-07 13:42:10 +0100

    add links to submit project

--- run-mlflow-retrain-model.sh
+++ run-mlflow-retrain-model.sh
@@ -1,3 +1,3 @@
 mlflow run https://github.com/thilo20/build-ml-pipeline-for-short-term-rental-prices.git \
-            -v 1.0.0 \
+            -v 1.0.1 \
             -P hydra_options="etl.sample='sample2.csv'"
